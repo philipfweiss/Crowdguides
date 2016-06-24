b9ff8b777c663f035f84d705d06c58e125a61c2a
philipfweiss <weissp68@stanford.edu>
2016-06-24 15:17:16 -0700

    add basic signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
   before_save {self.email.downcase!}
+  before_save {:default_email}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:name, presence: true, length: {maximum: 50})
   validates(:email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false})
   validates(:password, presence: true, length: {minimum: 6})
   has_secure_password
+
+  def default_email
+    self.email ||= 'example@railstutorial.org'
+  end
 
 end
