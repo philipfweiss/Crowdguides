df7dec14ffce14b64c09c43674522e4818be643f
philipfweiss <weissp68@stanford.edu>
2016-06-24 10:05:32 -0700

    Add validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ def setup
+   @user = User.new(name: "Philip Weiss", email: "weissp68@stanford.edu")
+ end
+
+  test "User is valid" do
+    @user.valid?
+  end
+
+  test "Name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "Email should be valid" do
+    @user.email = "      "
+    assert_not @user.valid?
+  end
+
+  test "Email should not be too long" do
+    @user.email = 'a'*255 + "example.com"
+    assert_not @user.valid?
+  end
+
+  test "Name should not be too long do" do
+    @user.name = 'a'*51
+    assert_not @user.valid?
+  end
 end
