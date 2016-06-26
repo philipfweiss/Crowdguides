7395436f836100933d40cacbfad8ed5534e3b57c
philipfweiss <weissp68@stanford.edu>
2016-06-26 16:14:17 -0700

    Create login form

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destory'
+
+
   root 'static_pages#home'
   resources :users
 end
