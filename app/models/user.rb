110729b24283dd20742f5a33f45a797863dbc463
philipfweiss <weissp68@stanford.edu>
2016-06-30 14:02:13 -0700

    change user schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:name, presence: true, length: {maximum: 50})
   validates(:email, presence: true, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX},
@@ -22,6 +25,7 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(self.remember_token))
   end
 
+
   def forget
     update_attribute(:remember_digest, nil)
   end
@@ -30,4 +34,15 @@ class User < ActiveRecord::Base
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
+  private
+
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
+    def downcase_email
+      self.email = email.downcase
+    end
 end
