5d721810c4ea4c915eaa21e2df4c58a4a364e935
philipfweiss <weissp68@stanford.edu>
2016-07-05 17:55:34 -0700

    add simple form for new guides

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,27 @@
 class GuidesController < ApplicationController
   def new
+    @guide = current_user.guides.build
+    @user = current_user
   end
+
+  def create
+    @guide = current_user.guides.build(guides_params)
+    if @guide.save
+      flash[:success] = "Guide created!"
+      redirect_to @user
+    else
+      flash[:failure] = "Failed to create your guide"
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+    def guides_params
+      params.require(:guide).permit(:title, :description)
+    end
+
 end
