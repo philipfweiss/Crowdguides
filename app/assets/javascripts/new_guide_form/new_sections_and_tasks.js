936a4a7c45d460277ca0c1dc959da7289ae86397
philipfweiss <weissp68@stanford.edu>
2016-07-07 16:42:59 -0700

    fuck around with jquery

--- app/assets/javascripts/new_guide_form/new_sections_and_tasks.js
+++ app/assets/javascripts/new_guide_form/new_sections_and_tasks.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 
     $('#new_section_button').on("click", function() {
-        $('#new_section_button').before("<div>Hello!</div>");
-    })
+        var addSection = '<div><div id="new_guide_title">New Section:</div> <input class="new_guide_section_edit" placeholder="Section Title:"> <input class="new_guide_section_edit" placeholder="Section Description:"> <input class="new_guide_task_edit" placeholder="Add a Task..."> <button type="button" class="btn btn-xs btn-success new_task_button" id="new_task_button"> + </button> </div>';
+        $('#new_section_button').before(addSection);
+    });
+
+    $('.new_task_button').on("click", function() {
+        var newTask = '<input class="new_guide_task_edit" placeholder="Add a Task...">';
+        $(this).before(newTask);
+    });
+
+
     }
 );
