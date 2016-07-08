6620a848b26f8250ccdb256df360c85a34f70340
philipfweiss <weissp68@stanford.edu>
2016-07-08 14:26:03 -0700

    refactor ideas

--- app/assets/javascripts/new_guide_form/new_sections_and_tasks.js
+++ app/assets/javascripts/new_guide_form/new_sections_and_tasks.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
+    var Guide = {title: "", description: "", sections: []};
+    var createSection;
+    var deleteSection;
+    var createTask;
+    var deleteTask;
+
+
+
+    createSection = function(sectionId) {
+        // Find the max key in the sections array and add 1
+        // Then, create a section with that ID
+
+    };
+
+    deleteSection = function(sectionId) {
+       // Delete a section given its section ID;
+    };
+
+    createTask = function(sectionId) {
+        // Finds the max task_id in the section and adds 1
+    };
+
+    deleteTask = function(sectionId, taskId) {
+        // Deletes a task with the largest section ID.
+    }
+
 
 });
