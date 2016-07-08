3533aa9d829964cd8ea566c45de1722fabb08892
philipfweiss <weissp68@stanford.edu>
2016-07-08 15:00:43 -0700

    add new javascript stuff

--- app/assets/javascripts/new_guide_form/new_sections_and_tasks.js
+++ app/assets/javascripts/new_guide_form/new_sections_and_tasks.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
-    var Guide = {title: "", description: "", sections: []};
+    var Guide = {title: "", description: "", sections: []}; // The section array is [[1,[Tasks]], [2, [Tasks]]] etc...
     var createSection;
     var deleteSection;
     var createTask;
     var deleteTask;
 
 
+    $(document).on("click", "#new_section_button", function() {
+        createSection();
+    });
 
-    createSection = function(sectionId) {
+
+    createSection = function() {
         // Find the max key in the sections array and add 1
-        // Then, create a section with that ID
+        var max = 0;
+        for(var section in Guide.sections) {
+            if(section[0] != null && section[0] > max) {
+                max = section[0];
+            }
+        }
 
+        // Then, create a section with that ID
+        Guide.sections.push([max+1, []]);
+        console.log("Shit!");
+        $('#new_section_button').before('<div>Hello!</div>');
     };
+
 
     deleteSection = function(sectionId) {
        // Delete a section given its section ID;
@@ -23,7 +37,7 @@ $(document).ready(function() {
 
     deleteTask = function(sectionId, taskId) {
         // Deletes a task with the largest section ID.
-    }
+    };
 
 
 });
