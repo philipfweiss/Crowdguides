96d6379117e7489d85ecfdca0f5538e82d352014
philipfweiss <weissp68@stanford.edu>
2016-07-08 10:53:12 -0700

    fix jquery functionality

--- app/assets/javascripts/new_guide_form/new_sections_and_tasks.js
+++ app/assets/javascripts/new_guide_form/new_sections_and_tasks.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
-    $('#new_section_button').on("click", function() {
-        var addSection = '<div><div id="new_guide_title">New Section:</div> <input class="new_guide_section_edit" placeholder="Section Title:"> <input class="new_guide_section_edit" placeholder="Section Description:"> <input class="new_guide_task_edit" placeholder="Add a Task..."> <button type="button" class="btn btn-xs btn-success new_task_button" id="new_task_button"> + </button> </div>';
+    // This creates a new section when you click the "new section" button.
+    $(document).on("click", '#new_section_button', function() {
+        var optionsButton = '<div class="dropdown intro_dropdown"> <button class="btn btn-xs dropdown-toggle intro_form_button" type="button" data-toggle="dropdown"> <span class="caret"></span></button> <ul class="dropdown-menu intro_form_menu"> <li ><a class="intro_form_blockers" href="#">Blockers: 0</a></li> <li ><a class="intro_form_warnings" href="#">Warnings: 0</a></li> <li ><a class="intro_form_tips" href="#">Tips: 0</a></li> </ul> </div>';
+        var addSection = '<div><div id="new_guide_title">New Section: '+ optionsButton + '</div>  <input class="new_guide_section_edit" placeholder="Section Title:"> <input class="new_guide_section_edit" placeholder="Section Description:"> <input class="new_guide_task_edit" placeholder="Add a Task..."> <button type="button" class="btn btn-xs btn-success new_task_button" id="new_task_button"> + </button> </div>';
         $('#new_section_button').before(addSection);
     });
 
-    $('.new_task_button').on("click", function() {
+    // This creates a new task when you click the "+" button.
+    $(document).on("click", ".new_task_button", function() {
         var newTask = '<input class="new_guide_task_edit" placeholder="Add a Task...">';
         $(this).before(newTask);
     });
