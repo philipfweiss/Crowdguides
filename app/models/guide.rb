db067ca61b40d06ae39900967f204e561228ffda
philipfweiss <weissp68@stanford.edu>
2016-07-13 12:46:35 -0700

    fix record

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,8 +1,4 @@
 class Guide < ActiveRecord::Base
-  searchable do
-    text :title
-  end
-
   belongs_to :user
   validates :title, presence: true, length: {maximum: 200}
   validates :description, presence: true, length: {maximum: 10000}
