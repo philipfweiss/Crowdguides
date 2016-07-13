857618934339b4d18a1bc9bba7fced4a7b983b6b
philipfweiss <weissp68@stanford.edu>
2016-07-13 12:01:32 -0700

    try out solr?

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,4 +1,8 @@
 class Guide < ActiveRecord::Base
+  searchable do
+    text :title
+  end
+
   belongs_to :user
   validates :title, presence: true, length: {maximum: 200}
   validates :description, presence: true, length: {maximum: 10000}
