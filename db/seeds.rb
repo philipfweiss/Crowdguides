c580bb08116c1bc9a6eccd6f0996dffe81da06a6
philipfweiss <weissp68@stanford.edu>
2016-07-14 13:05:31 -0700

    remove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
-
-if User.count > 6
-  users = User.order(:created_at).take(6)
-  50.times do
-    content = Faker::University.name + " Admissions Guide"
-    users.each { |user| user.guides.create!(title: content, description: "None!") }
-  end
-end
+#
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password,
+#                activated: true,
+#                activated_at: Time.zone.now)
+# end
+#
+# if User.count > 6
+#   users = User.order(:created_at).take(6)
+#   50.times do
+#     content = Faker::University.name + " Admissions Guide"
+#     users.each { |user| user.guides.create!(title: content, description: "None!") }
+#   end
+# end
