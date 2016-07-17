9775bd2535d68f9b6e0b591b6de31d3ed460fb55
philipfweiss <weissp68@stanford.edu>
2016-07-16 23:56:32 -0700

    change styling and allow for user forums

--- app/controllers/advice_controller.rb
+++ app/controllers/advice_controller.rb
@@ -6,6 +6,8 @@ class AdviceController < ApplicationController
     @guide = Guide.find(params[:guide_id])
     @id = params[:id]
     @advice = @guide.advices.find_by(inner_guide_id: params[:id])
+    @blockers = @advice.tidbits.where(:issue_type => 'blocker')
+
     redirect_to @guide if @advice.nil?
   end
 
