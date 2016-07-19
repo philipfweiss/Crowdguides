0eaee5667a46c004115365948a5f230926f4850e
philipfweiss <weissp68@stanford.edu>
2016-07-19 00:54:09 -0700

    offline users can now submit questions

--- app/controllers/tidbits_controller.rb
+++ app/controllers/tidbits_controller.rb
@@ -3,13 +3,11 @@ class TidbitsController < ApplicationController
     @tidbit_type = params[:tidbit_type]
     @guide = Guide.find(params[:guide_id])
     @advice = @guide.advices.find_by(inner_guide_id: params[:advice_id])
+    session[:last_guide] = params[:guide_id]
+    session[:last_advice] = params[:advice_id]
 
     unless @tidbit_type == 'tip' || @tidbit_type == 'issue' || @tidbit_type == 'question'
       redirect_to guide
-    end
-
-    unless logged_in?
-      redirect_to @guide
     end
 
 
@@ -17,20 +15,13 @@ class TidbitsController < ApplicationController
   end
 
   def create
-    unless logged_in?
-      redirect_to @guide
-    end
 
     @tidbit = Tidbit.new(tidbit_params)
     if @tidbit.save
       flash[:success] = "You Successfully Submitted Your Question"
     end
 
-    if logged_in?
-      redirect_to current_user
-    else
-      render 'new'
-    end
+    redirect_to "/guides/#{session[:last_guide]}/advice/#{session[:last_advice]}"
 
   end
 
