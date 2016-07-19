12bab0f2ed873c2bb05e581b3c862fc720935f99
philipfweiss <weissp68@stanford.edu>
2016-07-19 00:28:58 -0700

    users can now ask questions

--- app/controllers/tidbits_controller.rb
+++ app/controllers/tidbits_controller.rb
@@ -1,18 +1,43 @@
 class TidbitsController < ApplicationController
   def new
-    @guide_id = params[:guide_id]
-    @advice_id = params[:advice_id]
     @tidbit_type = params[:tidbit_type]
+    @guide = Guide.find(params[:guide_id])
+    @advice = @guide.advices.find_by(inner_guide_id: params[:advice_id])
+
+    unless @tidbit_type == 'tip' || @tidbit_type == 'issue' || @tidbit_type == 'question'
+      redirect_to guide
+    end
+
+    unless logged_in?
+      redirect_to @guide
+    end
+
+
     @tidbit = Tidbit.new
   end
 
   def create
+    unless logged_in?
+      redirect_to @guide
+    end
+
+    @tidbit = Tidbit.new(tidbit_params)
+    if @tidbit.save
+      flash[:success] = "You Successfully Submitted Your Question"
+    end
+
+    if logged_in?
+      redirect_to current_user
+    else
+      render 'new'
+    end
+
   end
 
   private
 
   def tidbit_params
     ## Can we make sure this doesn't break when another user tries to submit as someone else?
-    params.require(:tidbit).permit(:title, :contents, :issue_type, :user_id)
+    params.require(:tidbit).permit(:title, :contents, :issue_type, :advice_id, :user_id)
   end
 end
--- app/models/tidbit.rb
+++ app/models/tidbit.rb
@@ -1,4 +1,6 @@
 class Tidbit < ActiveRecord::Base
   belongs_to :advice
   belongs_to :user
+
+  #Add validations for this stuff!
 end
