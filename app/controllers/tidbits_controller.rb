be11bf0f052351e90fb6cda260f864f7f5e853b3
philipfweiss <weissp68@stanford.edu>
2016-07-18 23:52:30 -0700

    add tibit creation form

--- app/controllers/tidbits_controller.rb
+++ app/controllers/tidbits_controller.rb
@@ -2,5 +2,17 @@ class TidbitsController < ApplicationController
   def new
     @guide_id = params[:guide_id]
     @advice_id = params[:advice_id]
+    @tidbit_type = params[:tidbit_type]
+    @tidbit = Tidbit.new
+  end
+
+  def create
+  end
+
+  private
+
+  def tidbit_params
+    ## Can we make sure this doesn't break when another user tries to submit as someone else?
+    params.require(:tidbit).permit(:title, :contents, :issue_type, :user_id)
   end
 end
