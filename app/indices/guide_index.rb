ba0042654cfcaebf3e21c76e7b15da36be3ef095
philipfweiss <weissp68@stanford.edu>
2016-09-18 17:15:04 -0700

    remove guide indices

--- app/indices/guide_index.rb
+++ app/indices/guide_index.rb
@@ -1,12 +1,12 @@
-ThinkingSphinx::Index.define :guide, :with => :real_time do
-  # fields
-  indexes title, :sortable => true
-  indexes content, :sortable => true
-
-  # attributes
-  has title, :type => :text
-  has content, :type => :text
-  has user_id,  :type => :integer
-  has created_at, :type => :timestamp
-  has updated_at, :type => :timestamp
-end
+# ThinkingSphinx::Index.define :guide, :with => :real_time do
+#   # fields
+#   indexes title, :sortable => true
+#   indexes content, :sortable => true
+#
+#   # attributes
+#   has title, :type => :text
+#   has content, :type => :text
+#   has user_id,  :type => :integer
+#   has created_at, :type => :timestamp
+#   has updated_at, :type => :timestamp
+# end
