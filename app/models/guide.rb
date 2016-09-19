bd3ccd48890ff814c4bbd3f75a5974c412b88aa6
philipfweiss <weissp68@stanford.edu>
2016-09-18 17:22:16 -0700

    prevent dependent destroy

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,5 +1,5 @@
 class Guide < ActiveRecord::Base
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   has_many :advices, dependent: :destroy
   validates :title, presence: true, length: {maximum: 200}
   validates :description, presence: true, length: {maximum: 10000}
